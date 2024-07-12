12c82335a3ab0d066a18de54022728501b54a5e8
Aj8056 <53955617+Aj8056@users.noreply.github.com>
2024-07-12 17:37:06 -0400

    adding privacy policy

---
+++ pages/privacy.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const PrivacyPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>
+                Your privacy is important to us. This privacy policy outlines
+                the types of personal information we receive and collect when
+                you use our website and how we safeguard your information.
+            </p>
+            <p>
+                We may collect personal information such as your name, email
+                address, and phone number when you fill out a contact form or
+                subscribe to our newsletter. This information will only be used
+                for the purpose of communicating with you and providing the
+                services you requested.
+            </p>
+            <p>
+                We may also collect non-personal information such as your IP
+                address, browser type, and operating system to analyze trends,
+                administer the site, track user's movement, and gather broad
+                demographic information for aggregate use. This information is
+                not linked to any personally identifiable information.
+            </p>
+            <p>
+                We use cookies to personalize content and ads, to provide social
+                media features, and to analyze our traffic. We also share
+                information about your use of our site with our social media,
+                advertising, and analytics partners who may combine it with
+                other information that you've provided to them or that they've
+                collected from your use of their services.
+            </p>
+            <p>
+                Our website may contain links to other websites. Please be aware
+                that we are not responsible for the privacy practices of such
+                other sites. We encourage our users to be aware when they leave
+                our site and to read the privacy statements of each and every
+                website that collects personally identifiable information. This
+                privacy policy applies solely to information collected by this
+                website.
+            </p>
+            <p>
+                If you have any questions or concerns about our privacy policy,
+                please contact us.
+            </p>
+        </div>
+    )
+}
+
+export default PrivacyPage
